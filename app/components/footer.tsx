b8960c9966335948a507cb9d54e1bf96a201d3f3
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-05-30 14:48:48 +1000

    update footer with more linkies

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -30,6 +30,28 @@ export default function Footer() {
             <p className="ml-2 h-7">view source</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://yunz-dev.medium.com"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">medium</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://yunz.dev"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">portfolio</p>
+          </a>
+        </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300 text-center">
         {new Date().getFullYear()} MIT Licensed
