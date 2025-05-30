de89d9414df89059750145bbb6b349ead94e3db9
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-05-30 14:03:19 +1000

    meoww

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import { BlogPosts } from 'app/components/posts'
-
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+    <section className="flex flex-col items-center justify-center min-h-screen py-16">
+      <h1 className="mb-8 text-4xl font-bold tracking-tighter text-center">
+        Welcome to the World of Yunz
       </h1>
-      <p className="mb-4">
-        {`I am yunz 👋👋👋👋👋`}
+      <p className="mb-12 text-xl text-center">
+        {`Meoww 👋`}
       </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
+      <img 
+        src="images/cat.jpg"
+        alt="A cute cat"
+        className="w-full max-w-4xl rounded-lg shadow-2xl"
+      />
     </section>
   )
 }
