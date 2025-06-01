867b376d0c385ae0aee53b20e02202159943fa77
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-06-01 13:13:14 +1000

    update work

--- app/lib/work-data.ts
+++ app/lib/work-data.ts
@@ -15,27 +15,6 @@ export const workItems: WorkItem[] = [
     title_prefix: "Evening Shift at",
   },
   {
-    id: 2,
-    date_str: "01/06/2025",
-    location: "Krispy Kreme",
-    time_range: "09:00-17:00",
-    title_prefix: "Day Shift at",
-  },
-  {
-    id: 3,
-    date_str: "01/06/2025",
-    location: "Krispy Kreme",
-    time_range: "12:00-16:00",
-    title_prefix: "Afternoon Shift at",
-  },
-  {
-    id: 4,
-    date_str: "03/06/2025",
-    location: "Krispy Kreme",
-    time_range: "06:00-14:00",
-    title_prefix: "Morning Shift at",
-  },
-  {
     id: 5,
     date_str: "04/06/2025",
     location: "Krispy Kreme",
